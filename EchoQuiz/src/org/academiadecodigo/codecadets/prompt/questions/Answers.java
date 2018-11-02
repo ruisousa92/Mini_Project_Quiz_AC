0b023031e0cae85f3bb57979a2ff3c537ecf6780
ruisousa <rvcsousa@hotmail.com>
2018-11-02 19:06:24 -0100

    Answers added

---
+++ EchoQuiz/src/org/academiadecodigo/codecadets/prompt/questions/Answers.java
@@ -0,0 +1,6 @@
+package org.academiadecodigo.codecadets.prompt.questions;
+
+public class Answers {
+
+    private static final String[] ANSWERS_QUESTION_ONE = {"answer1","answer2","answer3","answer4"};
+}
