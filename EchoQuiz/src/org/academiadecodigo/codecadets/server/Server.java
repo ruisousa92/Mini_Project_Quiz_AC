d9854aef1cda743138ce44f15aa6ec95a19bfb8b
Jonathan Afonso <jonathan.ds.afonso@gmail.com>
2018-11-02 20:04:40 -0100

    server, initial commit

--- EchoQuiz/src/org/academiadecodigo/codecadets/server/Server.java
+++ EchoQuiz/src/org/academiadecodigo/codecadets/server/Server.java
@@ -1,16 +1,84 @@
 package org.academiadecodigo.codecadets.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Server {
 
+    private static final String DEFAULT_NAME = "";
+    private static final int MAXIMUM_CLIENT = 2;
 
-    public Server() {
+    private final ServerSocket serverSocket;
+    private final ExecutorService service;
+    private final List<ClientHandler> clients;
+
+    public Server(int portNumber) throws IOException {
+        this.serverSocket = new ServerSocket(portNumber);
+        this.service = Executors.newCachedThreadPool();
+        this.clients = Collections.synchronizedList(new LinkedList<>());
     }
 
     /**
-     * Inner Class
+     *
      */
-    public class ClientHandler {
+    public void start() {
+
+        int openedConnections = 0;
+
+        try {
+            while (!serverSocket.isClosed()) {
+                waitingForClientConnections(openedConnections);
+                openedConnections++;
+            }
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    /**
+     * Has a blocking method - accept(); - it will wait until a client connects to
+     * the corresponded port number.
+     * @param connections
+     * @throws IOException
+     */
+    public void waitingForClientConnections(int connections) throws IOException {
+
+        try {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Waiting for client connections");
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
 
 
+    /**
+     * Inner class of Client Handler
+     * handles and distributes sockets to a client/user each time they connect
+     */
+    public class ClientHandler implements Runnable {
+        private Socket socket;
+        private String name;
+        private PrintWriter printWriter;
+        private BufferedReader inputReader;
+
+        ClientHandler(Socket socket, String name) {
+            this.socket = socket;
+            this.name = name;
+        }
+
+
+        @Override
+        public void run() {
+
+        }
     }
 }
--- EchoQuiz/src/org/academiadecodigo/codecadets/server/ServerLauncher.java
+++ EchoQuiz/src/org/academiadecodigo/codecadets/server/ServerLauncher.java
@@ -1,8 +1,19 @@
 package org.academiadecodigo.codecadets.server;
 
+import java.io.IOException;
+
 public class ServerLauncher {
+
+    private static final int DEFAULT_PORT = 9090;
 
     public static void main(String[] args) {
 
+        try {
+            Server server = new Server(DEFAULT_PORT);
+            server.start();
+
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
     }
 }
