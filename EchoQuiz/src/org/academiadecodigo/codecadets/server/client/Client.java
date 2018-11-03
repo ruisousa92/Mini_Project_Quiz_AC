9f7f56cb0de61caa08e1895c15a504c2832a9334
Jonathan Afonso <jonathan.ds.afonso@gmail.com>
2018-11-02 23:00:34 -0100

    client initial commit

--- EchoQuiz/src/org/academiadecodigo/codecadets/server/client/Client.java
+++ EchoQuiz/src/org/academiadecodigo/codecadets/server/client/Client.java
@@ -1,6 +1,24 @@
 package org.academiadecodigo.codecadets.server.client;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
+
+    private Socket socket;
+
+    public Client(String host, int portNumber) throws IOException {
+        socket = new Socket(host,portNumber);
+    }
+
+
+    public void start() {
+
+
+    }
+
+
+    public
 
 
 
